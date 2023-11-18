6b3a484b7f6e2e91ef6fe2d2378883c2107d5b3a
jeremy delacruz <jeremy.delacruz.dev@gmail.com>
2023-11-18 11:44:11 +0800

    add loader ui

--- static/helpers/boosterSteps.js
+++ static/helpers/boosterSteps.js
@@ -18,39 +18,49 @@ export const addTimeGap = async (msec = 5000) => {
 export const addRandomTimeGap = async (min = 5, max = 10) => {
     await addTimeGap(generateNumberBetween(min, max) * 1000)
 }
-  
-export const getRandomProxy = async () => {
-    const proxyTxt = await loadTextFile('./proxies/proxies.txt')
-    let arr = proxyTxt.split('\n')
-
-    return arr[(Math.floor(Math.random() * arr.length))]
-}
 
 export const getRandomKeyword = (keywords) => {
     return keywords[(Math.floor(Math.random() * keywords.length))]
 }
   
 export const isForbiddenPage = (request) =>{
-    // Check for common HTTP status codes
     const response = request.response();
 
     if (response) {
         const statusCode = response.status();
         if (statusCode === 403) {
-        console.log(`Forbidden page request (403): ${request.url()}`);
-        // Optionally take action for 403 Forbidden pages
-        return true;
+            console.log(`Forbidden page request (403): ${request.url()}`);
+            return true;
         } else if (statusCode === 404) {
-        console.log(`Not Found page request (404): ${request.url()}`);
-        // Optionally take action for 404 Not Found pages
-        return true;
+            console.log(`Not Found page request (404): ${request.url()}`);
+            return true;
         } else if (statusCode === 500) {
-        console.log(`Internal Server Error page request (500): ${request.url()}`);
-        // Optionally take action for 500 Internal Server Error pages
-        return true;
+            console.log(`Internal Server Error page request (500): ${request.url()}`);
+            return true;
         }
     }
     return false;
+}
+
+const openProxiesFile = async () => {
+    const proxyTxt = await loadTextFile('./proxies/proxies.txt')
+    let arr = proxyTxt.split('\n')
+
+    return arr[(Math.floor(Math.random() * arr.length))]
+}
+  
+export const getRandomProxy = async () => {
+    let proxies = null;
+
+    while(proxies == null) {
+        try {
+            proxies = await openProxiesFile();
+        }catch ($error) {
+            console.log('error on opening the proxies files');
+        }
+    }
+
+    return proxies
 }
 
 export const downloadProxies = async () => {
