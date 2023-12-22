3d55f827b3776aa82970f31fef8aaccd365cf42d
jeremy delacruz <jeremy.delacruz.dev@gmail.com>
2023-12-23 00:13:05 +0800

    close the browser

--- src/helpers/boosterSteps.js
+++ src/helpers/boosterSteps.js
@@ -103,8 +103,7 @@ export const scrollToRandomClass = async (page, elementClass, browser) => {
         element.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });
     }, randomProduct);
   }catch(error){
-    browser.close();
-    console.log(error);
+    return;
   }
 };
 
