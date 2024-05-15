4d81c9780071add70726b2f15eaf70ba19fc2ceb
jeremy delacruz <jeremy.delacruz.dev@gmail.com>
2024-05-15 18:46:31 +0800

    make the booster faster

--- src/helpers/boosterSteps.js
+++ src/helpers/boosterSteps.js
@@ -110,7 +110,7 @@ export const scrollToRandomClass = async (page, elementClass, browser) => {
         element.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });
     }, randomProduct);
   }catch(error){
-    return;
+    browser.close();
   }
 };
 
