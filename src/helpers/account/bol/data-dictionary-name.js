b74e9c4567edcc4059b5d86e800ac2088820bd2a
jeremy delacruz <jeremy.delacruz.dev@gmail.com>
2024-06-22 10:54:56 +0800

    account information generator

---
+++ src/helpers/account/bol/data-dictionary-name.js
@@ -0,0 +1,31 @@
+// List of common Dutch first names, middle names, and last names
+
+export const dutchFirstNames = [
+  'Jan', 'Pieter', 'Hendrik', 'Kees', 'Maarten', 'Lars', 
+  'Sanne', 'Fleur', 'Marieke', 'Anouk', 'Willem', 'Eva', 
+  'Sophie', 'Max', 'Julia', 'Ruben', 'David', 'Emma', 
+  'Thomas', 'Lisa', 'Tim', 'Anna', 'Rick', 'Sarah', 
+  'Michael', 'Michelle', 'Daniel', 'Laura', 'Peter', 
+  'Caroline', 'Jeroen', 'Monique', 'Rik', 'Nina', 
+  'Mark', 'Diana', 'Lucas', 'Marianne', 'Sam', 'Lotte', 
+  'Frank', 'Amber', 'Vincent', 'Linda', 'Erik', 'Eline'
+  ];
+  
+export const dutchMiddleNames = [
+  'van', 'de', 'ter', 'te', 'van der', 'den', 
+  'van de', 'de la', 'op', 'aan', '', 'ten', 
+  'in den', 'op den', 'op'  
+];
+  
+export const dutchLastNames = [
+  'Jansen', 'de Vries', 'Bakker', 'Visser', 'Smit', 
+  'de Jong', 'Mulder', 'de Boer', 'Vos', 'Peeters', 
+  'Hendriks', 'van Dijk', 'van der Meer', 'van den Berg', 
+  'Kok', 'Jacobs', 'van Leeuwen', 'Bos', 'Veldhuis', 
+  'Willems', 'van der Velden', 'de Graaf', 'Huisman', 
+  'Koning', 'Vermeer', 'Schouten', 'de Bruijn', 'Groen', 
+  'Wouters', 'van Loon', 'Post', 'Mol', 'Brouwer', 
+  'de Wit', 'Jonker', 'van der Wal', 'Kuijpers', 
+  'Sanders', 'van Beek', 'Prins', 'Martens', 'van Rijn', 
+  'de Groot', 'van der Heijden', 'Dekker', 'Vermeulen'
+];
